2913de67cdd2883f51028cd62f725cb423ecf6c3
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2018-10-16 22:22:24 +0800

    Change wrong source code

--- traffic-light/src/App.js
+++ traffic-light/src/App.js
@@ -2,40 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-const state_machine = {
-  green: 'yellow',
-  yellow: 'red',
-  red: 'green'
-};
-
-let current_state = 'green';
-
-const update_light = (state)=>{
-  current_state = state;
-  // update color
-
-};
-
-const Transition = (state)=>{
-
-}
-
-const CircleLight = (color)=>{
-  return(
-    <div className = "circle" style={{color: color}}> </div>);
-};
-
-
-function Traffic_light() {
-  return(
-  <div className = "trafficBox">
-    <CircleLight color='green'/>
-    <CircleLight color='yellow'/>
-    <CircleLight color='red'/>
-  </div>);
-};
-
 class App extends Component {
   render() {
     return (
@@ -43,7 +9,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />          
         </header>
-        <Traffic_light />
       </div>    
     )}
 }
--- traffic-light/src/index.js
+++ traffic-light/src/index.js
@@ -4,6 +4,86 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+class Traffic extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            seconds: 0,
+            red: {backgroundColor: "#282c34"},
+            yellow: {backgroundColor: "#282c34"},
+            green: {backgroundColor: "#282c34"}
+        };
+
+        this.map_color = {
+            red: {backgroundColor: "#ff4135"},
+            yellow: {backgroundColor: "#eca231"},
+            green: {backgroundColor: "#00d480"},
+            off: {backgroundColor: "#282c34"}
+        };
+
+    }
+  
+    tick() {
+      this.setState(state => ({
+        seconds: state.seconds + 1
+      }));
+      this.updateTrafficLight()
+    }
+  
+    componentDidMount() {
+      this.interval = setInterval(() => this.tick(), 1000);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+
+    updateTrafficLight(){
+        var remainder = this.state.seconds%10
+        switch (remainder) {
+            case (0):
+                this.setState(state => ({
+                    red: {backgroundColor: this.map_color.red},
+                    yellow: {backgroundColor: this.map_color.off},
+                    green: {backgroundColor: this.map_color.off}
+                }));
+                break;
+            case (3):
+                this.setState(state => ({
+                    red: {backgroundColor: this.map_color.off},
+                    yellow: {backgroundColor: this.map_color.yellow},
+                    green: {backgroundColor: this.map_color.off}
+                }));
+                break;
+            case (6):
+                this.setState(state => ({
+                    red: {backgroundColor: this.map_color.off},
+                    yellow: {backgroundColor: this.map_color.off},
+                    green: {backgroundColor: this.map_color.green}
+                }));
+                break;
+            default:
+                console.log("None", remainder)
+                break;
+        }
+        
+    }
+  
+    render() {
+      return (
+        <div className="traffic">
+        <div className= "circle" style = {this.state.red}></div>
+        <div className= "circle" style = {this.state.yellow}></div>
+        <div className= "circle" style = {this.state.green}>Seconds: {this.state.seconds%10}</div>
+        </div>
+      );
+    }
+
+  }
+  
+ReactDOM.render(<Traffic />, document.getElementById('traffic-light'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
